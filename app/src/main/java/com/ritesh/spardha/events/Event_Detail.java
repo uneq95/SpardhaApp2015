322cda4444e7d14be8cdce944271d9a9a1351a5b
unknown <abhishek.ramakant.che14@iitbhu.ac.in>
2015-10-05 00:13:26 +0530

    events detail updated

--- app/src/main/java/com/ritesh/spardha/events/Event_Detail.java
+++ app/src/main/java/com/ritesh/spardha/events/Event_Detail.java
@@ -46,7 +46,7 @@ public class Event_Detail extends AppCompatActivity {
     int url_no;
     String address = "http://www.spardha.co.in/file/";
     String[] RuleBookUrl = {"athletics.pdf", "badminton.pdf", "basketball.pdf", "boxing.pdf", "carrom.pdf", "chess.pdf", "cricket.pdf", "football.pdf", "handball.pdf", "hockey.pdf",
-            "kabaddi.pdf", "kho-kho.pdf", "squash.pdf", "tae-kwon-do.pdf", "tennis.pdf", "table-tennis.pdf", "volleyball.pdf", "lifting.pdf", "winner-athletics.pdf", "winner-tae-kwon-do.pdf"};
+            "kabaddi.pdf", "kho-kho.pdf", "lifting.pdf","squash.pdf", "tae-kwon-do.pdf", "tennis.pdf", "table-tennis.pdf", "volleyball.pdf", "lifting.pdf", "winner-athletics.pdf", "winner-tae-kwon-do.pdf"};
 
     Toolbar toolbar;
 
@@ -170,7 +170,7 @@ public class Event_Detail extends AppCompatActivity {
 
                 String final_url = address + RuleBookUrl[url_no];
 
-                if (req_no == 4 || req_no == 5 || req_no == 13||req_no==19) {
+                if (req_no == 4 || req_no == 5 ||req_no==19) {
                     Toast.makeText(getBaseContext(), "Will be Uploaded Soon", Toast.LENGTH_LONG).show();
                 } else {
 
--- app/src/main/java/com/ritesh/spardha/events/EventsDb.java
+++ app/src/main/java/com/ritesh/spardha/events/EventsDb.java
@@ -26,20 +26,20 @@ public class EventsDb {
 
     public static final String DATABASE_NAME  = "sports_detail";
     public static final String DATABASE_TABLE = "eventdetail";
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 5;
 
     private  final Context context;
     private SQLiteDatabase sportsdatabase;
     public DBHelper dbhelperobject;
 
     String[] categorysportsname = {"Athletics","Badminton","BasketBall","Boxing","Carrom","Chess","Cricket","FootBall","HandBall","Hockey","Kabaddi","KhoKho","Power Lifting","Squash","TaeKwonDo","Tennis","TableTennis","VolleyBall","WeightLifting"};
-    String[] categorywinner = {"Click to Download","IIT Kharagpur","BRCM CET(Boys),\n\nMITS(Girls)","---","UGI(Boys),\n\nKIT(Girls)","IIT BHU","IIT BHU","IIT BHU","IIT BHU","MERI Kolkatta","IIT BHU","IIT BHU","---","GEC Ajmer","Click To Download","Shiv Nadar","IIT Roorkee","Shri Ram College","---"};
-    String[] categoryrunnerup = {"Click to Download","IIT BHU","IIT BHU(Boys),\n\nIIT BHU(Girls)","---","BBD Lucknow(Boys),\n\nUGI(Girls)","IET Lucknow","BBD Lucknow","---","NIT Raipur","IIT BHU","YMCA Faridabad","IIT Mumbai","---","IIT BHU","Click to download","IIT BHU","SNU Delhi","IIT BHU","--"};
+    String[] categorywinner = {"Click to Download","IIT Kharagpur","BRCM CET(Boys),\n\nMITS(Girls)","---","UGI(Boys),\n\nKIT(Girls)","IIT BHU","IIT BHU","IIT BHU","IIT BHU","MERI Kolkatta","IIT BHU","IIT BHU(Boys)\n\nIIT BHU(Girls)","---","GEC Ajmer","Click To Download","Shiv Nadar(Boys),\n\nMITS(Girls)","IIT Roorkee(Boys)\n\nMITS(Girls)","Shri Ram College(Boys)\n\nShri Ram College(Girls)","---"};
+    String[] categoryrunnerup = {"Click to Download","IIT BHU","IIT BHU(Boys),\n\nIIT BHU(Girls)","---","BBD Lucknow(Boys),\n\nUGI(Girls)","IET Lucknow","BBD Lucknow","---","NIT Raipur","IIT BHU","YMCA Faridabad","IIT Mumbai\n\nIET Lucknow(Girls)","---","IIT BHU","Click to download","IIT BHU(Boys),\n\nIET Lucknow(Girls)","SNU Delhi(Boys),\n\nIIT BHU(Girls)","IIT BHU(Boys)\n\nMITS(GIRLS)","--"};
     String[] categoryrulebook = {"Athletics","Badminton","BasketBall","Boxing","Carrom","Chess","Cricket","FootBall","HandBall","Hockey","Kabaddi","KhoKho","Powerlifting","Squash","TaeKwonDo","Tennis","TableTennis","VolleyBall","WeightLifting"};
-    String[] categorycontacta = {"Gaurav  Sharma","Abhishek Dhananjaya","Mohammad Shahbaz","---","---","Gopal Garg ","Venkat Subhash ","Mayank kumar Morya","---","Suyash Tripathi","---","Nilabh Nandan","--","Siddhartha bhargava","Prabhat Srivastava","Samar Khan","Dhaval Dalia","Shivam jaiswal","---"};
-    String[] categorycontactb = {"---","---","Abhishek Dhayal","---","---","---","Dhruv Shikhar","Satyanand Tiwari","---","Prashant Kumar Singh","---","Prashanth Sai","---","---","---","---","---","Abhijit Mondal","---"};
-    String[] contacta = {"+917860561883","+919532892608","+918601546468","---","---","+919897232528","+918953723461","+919919546976","---","+918953880553","---","+917752957824","---","+917275588982","+919450675543","+919889203242","+917753069418","+919795637067","---"};
-    String[] contactb = {"---","---","+919473791571","---","---","---","+917376306960","+918960999789","---","+919616036867","---","+919492992254","---","---","---","---","---","+919454240708","---"};
+    String[] categorycontacta = {"Gaurav  Sharma","Abhishek Dhananjaya","Mohammad Shahbaz","---","---","Gopal Garg ","Venkat Subhash ","Mayank kumar Morya","Vaibhav Goyal","Suyash Tripathi","Shiva kumar naik","Nilabh Nandan","KaushikKonwar","Siddhartha bhargava","Prabhat Srivastava","Samar Khan","Dhaval Dalia","Shivam jaiswal","KaushikKonwar"};
+    String[] categorycontactb = {"---","---","Abhishek Dhayal","---","---","---","Dhruv Shikhar","Satyanand Tiwari","---","Prashant Kumar Singh","Shiva kumar chauhan","Prashanth Sai","Sumit Swarnamaya","---","---","---","---","Abhijit Mondal","Sumit Swarnamaya"};
+    String[] contacta = {"+917860561883","+919532892608","+918601546468","---","---","+919897232528","+918953723461","+919919546976","+919161524903","+918953880553","+918418080373","+917752957824","+919621748627","+917275588982","+919450675543","+919889203242","+917753069418","+919795637067","+919621748627"};
+    String[] contactb = {"---","---","+919473791571","---","---","---","+917376306960","+918960999789","---","+919616036867","+918009148763","+919492992254","+918009589847","---","---","---","---","+919454240708","+918009589847"};
 
     public EventsDb(Context context) {
         this.context = context;
